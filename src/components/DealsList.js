914d9f89f4b4c954cf4a977c59bc87b7cedefe87
Hani <msowe.feha@gmail.com>
2018-07-18 11:38:52 +0300

    fa icons added

--- src/components/DealsList.js
+++ src/components/DealsList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FaEdit from 'react-icons/lib/fa/edit';
+import FaTrash from 'react-icons/lib/fa/trash';
 
 const thumbnail ='/img/placeholder_50x50.jpg';
 
 class DealsList extends Component {
+
+  editDeal = (e) => {console.log('editDeal', e.target.value);}
+  deleteDeal = (e) => {console.log('deleteDeal', e.target.value);}
+
     render() {
       const deals = this.props.deals || [];
       return deals.map(({ dealId, dealCategory, dealPartner, dealPrice }) => (
-        <Link key={dealId} className="list-group-item list-group-item-action p-1" to={`/deal/${dealId}`}>
+        <div key={dealId} className="list-group-item list-group-item-action p-1">
         <div className="d-flex justify-content-between">
           <span>
             <img src={thumbnail} alt={dealPartner}/>
@@ -15,11 +21,12 @@ class DealsList extends Component {
             <strong>{dealPrice}</strong>
           </span>
           <span>
-            <span>del</span>
-            <span className="d-inline p-2">{dealCategory}</span>
+            <Link to=''><FaEdit value={dealId} className="m-1 grey" onClick={this.editDeal}/></Link>
+            <Link to=''><FaTrash value={dealId} className="m-1 red" onClick={this.deleteDeal}/></Link>
+            <h6 className="badge badge-primary p-2 mt-3 mr-2 rounded">{dealCategory}</h6>
           </span>
         </div>
-      </Link>
+      </div>
     ))
   }
 }
